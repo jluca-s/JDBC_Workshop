dd5d37bd446e9469c29eeebb281da16dd740cba2
Joao Goncalves <joaolucasgc@icloud.com>
2021-11-16 21:54:58 -0300

    Criando metodos de instancia Seller e Department

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -52,17 +52,9 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
-			Department dep = new Department();
-			dep.setId(rs.getInt("DepartmentId"));
-			dep.setName(rs.getString("DepName"));
+			Department dep = instanciateDepartment(rs);
 			
-			Seller seller = new Seller();
-			seller.setId(rs.getInt("Id"));
-			seller.setName(rs.getString("Name"));
-			seller.setEmail(rs.getString("Email"));
-			seller.setBaseSalary(rs.getDouble("BaseSalary"));
-			seller.setBirthDate(rs.getDate("BirthDate"));
-			seller.setDepartment(dep);
+			Seller seller = instanciateSeller(rs, dep);
 			
 			return seller;
 			}
@@ -77,6 +69,24 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 		
 		
+	}
+
+	private Seller instanciateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setDepartment(dep);
+		return seller;
+	}
+
+	private Department instanciateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
